773e9b09350e9974f163c63f085d01b7666116de
Solobored <josneiba10@gmail.com>
2025-01-06 18:31:17 -0300

    w02 grades

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage(no % syimbol) ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        string letter = ""; 
+        string sign = "";  
+
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+    
+        if (letter != "F") 
+        {
+            int lastDigit = percentage % 10;
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+
+            if (letter == "A" && sign == "+")
+            {
+                sign = "";
+            }
+        }
+
+        if (percentage == 100)
+        {
+            letter = "A";
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Better luck next time.");
+        }
+       
     }
 }
