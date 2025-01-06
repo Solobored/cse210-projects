7902362ed0ff6d74875126569f361b890e29fe57
Solobored <josneiba10@gmail.com>
2025-01-06 17:30:22 -0300

    Finished prep 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine().Trim();
+        Console.Write("What is yout last name? ");
+        string last = Console.ReadLine().Trim();
+        Console.Write($"Your name is {last}, {name} {last}.");
+
     }
 }
