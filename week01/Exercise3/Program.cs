c3f0a8b7c4b23a5c9e66bd30f2a6f8b09f0f9619
Solobored <josneiba10@gmail.com>
2025-01-06 21:48:36 -0300

    w03 guess number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random(); 
+        bool playAgain = true; 
+
+        while (playAgain)
+        {
+            int magicNumber = random.Next(1, 101); 
+            int guess = 0; 
+            int attempts = 0; 
+
+            Console.WriteLine("Welcome to the Guess My Number game!");
+
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string input = Console.ReadLine();
+
+
+                if (int.TryParse(input, out guess))
+                {
+                    attempts++;
+
+                    if (guess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You guessed it! The magic number was {magicNumber}.");
+                        Console.WriteLine($"It took you {attempts} attempts to guess correctly.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+
+            if (response != "yes" && response != "y")
+            {
+                playAgain = false; 
+                Console.WriteLine("Thanks for playing! Goodbye!");
+            }
+        }
     }
 }
